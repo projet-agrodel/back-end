ff8102a5ee1ed2851bb3aa776e2c901dea5f8007
yLexter <lucasmaia5954@gmail.com>
2025-03-27 17:09:28 -0300

    refactor: melhoria de codigos e correções de bugs

--- app/config.py
+++ app/config.py
@@ -2,8 +2,9 @@ import os
 from datetime import timedelta
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'sua-chave-secreta-aqui'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'mysql://user:password@localhost/dbname'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY') or 'jwt-secret-key'
+    SQLALCHEMY_ECHO=True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1) 
--- run.py
+++ run.py
@@ -1,11 +1,14 @@
-from app import create_app
 from dotenv import load_dotenv
+from app import create_app
 import os
 
 load_dotenv()
+
+os.environ["DATABASE_URL"] = "sqlite:///db-teste.db"
 
 app = create_app()
 
 if __name__ == '__main__':
     debug = os.getenv('FLASK_ENV') == 'development'
-    app.run(debug=debug) 
+    app.run(debug=debug)
+    
