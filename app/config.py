99dabda1711733dbe2e9353800fef46d6185128b
Deyvid Jerônimo <davidjmacedo@hotmail.com>
2025-05-04 21:22:09 -0300

    refactor: atualização na configuração do banco de dados e melhorias nas relações entre modelos

--- app/config.py
+++ app/config.py
@@ -4,7 +4,9 @@ from datetime import timedelta
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), '../instance/app.db')
+        
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SQLALCHEMY_ECHO=True
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1) 
+    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
--- app/models/product.py
+++ app/models/product.py
@@ -1,5 +1,6 @@
-from app import db
+from ..extensions import db
 from datetime import datetime
+from .category import Category
 
 class Product(db.Model):
     __tablename__ = 'produto'
@@ -9,14 +10,19 @@ class Product(db.Model):
     description = db.Column(db.String(255), nullable=False)
     price = db.Column(db.Numeric(10,2), nullable=False)
     stock = db.Column(db.Integer, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('categoria.id'), nullable=True)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow, onupdate=datetime.utcnow)
     
+    category = db.relationship('Category', back_populates='products')
+    
     def to_dict(self):
+        category_data = { 'id': self.category.id, 'name': self.category.name } if self.category else None
         return {
             'id': self.id,
             'name': self.name,
             'description': self.description,
             'price': float(self.price),
-            'stock': self.stock
+            'stock': self.stock,
+            'category': category_data
         } 
