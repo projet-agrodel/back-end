1dfaf143ff508de640fbf6af545ee3f07c141a9d
Deyvid Jerônimo <davidjmacedo@hotmail.com>
2025-05-20 22:05:46 -0300

    feat: adição da rota de análise administrativa e ajustes na configuração de envio de email

--- app/config.py
+++ app/config.py
@@ -11,13 +11,11 @@ class Config:
     SQLALCHEMY_ECHO=True
     JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
     FRONTEND_URL = os.environ.get('FRONTEND_URL') or 'http://localhost:3000'
-    # SENDGRID_API_KEY = os.environ.get('SENDGRID_API_KEY') # Remover SendGrid
     
-    # Configurações do Flask-Mail
-    MAIL_SERVER = os.environ.get('MAIL_SERVER') # Ex: 'smtp.gmail.com'
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
     MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587) # Ex: 587 (TLS) ou 465 (SSL)
-    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', '1', 't'] # Usar TLS? (Gmail usa)
-    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in ['true', '1', 't'] # Usar SSL?
-    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') # Seu email remetente
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') # Senha do seu email (ou senha de app para Gmail)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS', 'true').lower() in ['true', '1', 't'] 
+    MAIL_USE_SSL = os.environ.get('MAIL_USE_SSL', 'false').lower() in ['true', '1', 't']
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') 
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') 
     MAIL_DEFAULT_SENDER = os.environ.get('MAIL_DEFAULT_SENDER') or 'nao-responda@agrodel.com'
